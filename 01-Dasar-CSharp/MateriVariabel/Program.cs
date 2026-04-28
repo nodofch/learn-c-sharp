b4529ca03906441e182b611ad9d51709b4243058
nodofch <nodochisika13@gmail.com>
2026-04-29 01:46:03 +0700

    Belajar tipe data dasar C#

---
+++ 01-Dasar-CSharp/MateriVariabel/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+string namaKarakter = "Nodofch";
+int healthPoint = 100;
+float movementSpeed = 7.5f;
+bool isAlive = true;
+
+Console.WriteLine("=== STATUS HERO ===");
+Console.WriteLine("Nama     : " + namaKarakter);
+Console.WriteLine("Health   : " + healthPoint);
+Console.WriteLine("Speed    : " + movementSpeed);
+Console.WriteLine("Hidup    : " + isAlive);
+Console.WriteLine("====================");
+
+healthPoint = healthPoint - 20;
+Console.WriteLine("\nTerkena Serangan! HP Sekarang: " + healthPoint);
